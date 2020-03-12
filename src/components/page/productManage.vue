0b4d7982d118d58fcf11c55e891486a5429002ea
yml11 <1831773329@qq.com>
2020-03-12 10:37:39 +0800

    图片

--- src/components/page/productManage.vue
+++ src/components/page/productManage.vue
@@ -5,7 +5,36 @@
                 <el-breadcrumb-item><i class="el-icon-lx-copy"></i> 商品管理</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
-        
+        <el-table
+    :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+    style="width: 100%">
+    <el-table-column
+      label="Date"
+      prop="date">
+    </el-table-column>
+    <el-table-column
+      label="Name"
+      prop="name">
+    </el-table-column>
+    <el-table-column
+      align="right">
+      <template slot="header" slot-scope="scope">
+        <el-input
+          v-model="search"
+          size="mini"
+          placeholder="输入关键字搜索"/>
+      </template>
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
     </div>
 </template>
 
